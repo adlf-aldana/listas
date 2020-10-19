2e81170a5247ad2d9238256271172e5520b6342a
adolfo <adlf.aldana@gmail.com>
2020-10-19 08:42:11 -0400

    navegacion entre pantallas

--- src/app/Pages/tab1/tab1.page.ts
+++ src/app/Pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 import { Lista } from "src/app/models/lista.model";
 import { DeseosService } from "src/app/services/deseos.service";
 
@@ -9,7 +10,10 @@ import { DeseosService } from "src/app/services/deseos.service";
 })
 export class Tab1Page {
   listas: Lista[];
-  constructor(private deseosService: DeseosService) {
+  constructor(private deseosService: DeseosService, private router: Router) {
     this.listas = deseosService.listas;
+  }
+  agregarLista() {
+    this.router.navigateByUrl("/tabs/tab1/agregar");
   }
 }
