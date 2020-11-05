94bbbe6095c1bba74e4299972132145d5db21e39
adolfo <adlf.aldana@gmail.com>
2020-11-05 13:07:52 -0400

    editar el titulo de la lista

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit, ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
+import { AlertController, IonList } from "@ionic/angular";
 import { Lista } from "src/app/models/lista.model";
 import { DeseosService } from "src/app/services/deseos.service";
 
@@ -9,10 +10,14 @@ import { DeseosService } from "src/app/services/deseos.service";
   styleUrls: ["./listas.component.scss"],
 })
 export class ListasComponent implements OnInit {
-  listas: Lista[];
+  @ViewChild(IonList) lista: IonList;
   @Input() terminada: true;
-  constructor(private deseosService: DeseosService, private router: Router) {
-  }
+  listas: Lista[];
+  constructor(
+    public deseosService: DeseosService,
+    private router: Router,
+    private alertController: AlertController
+  ) {}
 
   ngOnInit() {}
 
@@ -28,5 +33,46 @@ export class ListasComponent implements OnInit {
   borrarLista(lista: Lista) {
     this.deseosService.borrarLista(lista);
     this.deseosService.guardarStorage();
+  }
+
+  async editarLista(lista: Lista) {
+    // this.router.navigateByUrl("/tabs/tab1/agregar");
+    const alert = await this.alertController.create({
+      header: "Editar Lista",
+      inputs: [
+        {
+          name: "titulo",
+          type: "text",
+          value: lista.titulo,
+          placeholder: "Nombre de la lista",
+        },
+      ],
+      buttons: [
+        {
+          text: "Cancelar",
+          role: "cancel",
+          handler: () => {
+            this.lista.closeSlidingItems();
+
+          },
+        },
+        {
+          text: "Actualizar",
+          handler: (data) => {
+            if (data.titulo.length === 0) {
+              return;
+            }
+            // const listaId = this.deseosService.crearLista(data.titulo);
+            lista.titulo = data.titulo;
+            // Tengo que crear la lista
+            // this.router.navigateByUrl(`/tabs/tab1/agregar/${listaId}`);
+            this.deseosService.guardarStorage();
+            this.lista.closeSlidingItems();
+          },
+        },
+      ],
+    });
+
+    alert.present();
   }
 }
