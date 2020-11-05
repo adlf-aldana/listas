58bd0710609f3581c558ed9b1d7f45bdbaa1a5b0
adolfo <adlf.aldana@gmail.com>
2020-11-05 09:19:30 -0400

    componentes listas

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Lista } from "src/app/models/lista.model";
+import { DeseosService } from "src/app/services/deseos.service";
 
 @Component({
-  selector: 'app-listas',
-  templateUrl: './listas.component.html',
-  styleUrls: ['./listas.component.scss'],
+  selector: "app-listas",
+  templateUrl: "./listas.component.html",
+  styleUrls: ["./listas.component.scss"],
 })
 export class ListasComponent implements OnInit {
-
-  constructor() { }
+  listas: Lista[];
+  @Input() terminada: true;
+  constructor(private deseosService: DeseosService, private router: Router) {
+    this.listas = deseosService.listas;
+  }
 
   ngOnInit() {}
 
+  listaSeleccionada(lista: Lista) {
+    if (this.terminada) {
+      // Tengo que crear la lista
+      this.router.navigateByUrl(`/tabs/tab2/agregar/${lista.id}`);
+    } else {
+      this.router.navigateByUrl(`/tabs/tab1/agregar/${lista.id}`);
+    }
+  }
 }
